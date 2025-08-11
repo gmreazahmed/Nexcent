ab02bd6192e0bb4c7774ff9a984ade3e76650f26
Nasif-Rahman <nasifurrahman2002@gmail.com>
2025-08-11 16:33:29 +0600

    b

---
+++ src/components/Section2.jsx
@@ -0,0 +1,12 @@
+
+
+const Section2 = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Our Clients</h1>
+      <p className="mt-2 text-gray-600">We have been working with some Fortune 500+ clients</p>
+    </div>
+  )
+}
+
+export default Section2
