6e8b336e571b2ded97adf69fd7698537d3ec638e
Nasif-Rahman <nasifurrahman2002@gmail.com>
2025-08-11 15:51:07 +0600

    boss

---
+++ src/components/Section1.jsx
@@ -0,0 +1,39 @@
+
+import Logo from './image/Logo.png';
+import Hero from './image/Hero.png';
+
+const Section1 = () => {
+  return (
+    <>
+    <div>
+       <nav className="flex items-center justify-between p-4 bg-white border-b border-gray-200">
+      <div className="flex items-center">
+        <img src={Logo} alt="Logo" className="" />
+        
+      </div>
+      <div className="space-x-6">
+        <a href="#" className="text-gray-600 hover:text-gray-900">Home</a>
+        <a href="#" className="text-gray-600 hover:text-gray-900">Features</a>
+        <a href="#" className="text-gray-600 hover:text-gray-900">Community</a>
+        <a href="#" className="text-gray-600 hover:text-gray-900">Blog</a>
+        <a href="#" className="text-gray-600 hover:text-gray-900">Pricing</a>
+      </div>
+      <a href="#" className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">Register Now →</a>
+    </nav>
+    </div>
+    <div>
+        <h1 className="htagg">Lessons and insights </h1>
+        <h1 className="htag2">from 8 years</h1>
+        <p className="mt-2 text-gray-600">Where to grow your business as a photographer: site or social media?</p>
+
+         <div className="flex items-center">
+        <img src={Hero} alt="Hero" className="" />
+        
+      </div>
+    </div>
+
+    </>
+  )
+}
+
+export default Section1
